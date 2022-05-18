812ad086a58f757514f38dfa69b14f8d340a0247
Siddharth <siddharth.pawar@tikaj.com>
2022-05-18 22:50:24 +0530

    Scorecard added

---
+++ src/components/QuizScoreCard/QuizScoreCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import classes from './QuizScoreCard.module.css';
+
+const QuizScoreCard = props => {
+    const {
+        answerDetails,
+        questions
+    } = props;
+
+    console.log(props);
+
+    let scoreCardRows = answerDetails.map(answerDetail => {
+        return (
+            <tr 
+                className={classes.ScoreCardRow} 
+                key={answerDetail.index}
+                style={{
+                    backgroundColor: (
+                        answerDetail.actualAnswer === answerDetail.submittedAnswer ? 
+                        "lightgreen" : 
+                        "indianred"
+                    )
+                }}
+            >
+                <td >{answerDetail.index + 1}</td>
+                <td 
+                    className={classes.ScoreCardColumn}
+                >
+                    {
+                     questions[answerDetail.index].operand1 + " " +
+                     questions[answerDetail.index].operator + " " +
+                     questions[answerDetail.index].operand2
+                    } 
+                </td>
+                
+                <td className={classes.ScoreCardColumn}>
+                    {answerDetail.actualAnswer}
+                </td>
+
+                <td className={classes.ScoreCardColumn}>
+                    {answerDetail.submittedAnswer}
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <table className={classes.ScoreCard}>
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Question</th>
+                    <th>Your Answer</th>
+                    <th>Correct Answer</th>
+                </tr>
+            </thead>
+            <tbody>
+                {scoreCardRows}
+            </tbody>
+        </table>
+    );
+}
+
+export default QuizScoreCard;
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,48 +2,54 @@ import React, { Component } from 'react';
 import classes from './Quiz.module.css';
 
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
+import QuizScoreCard from '../../components/QuizScoreCard/QuizScoreCard';
 
 class Quiz extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            questions: [
-                {
-                    operand1: 2,
-                    operand2: 3,
-                    operator: "+"
-                },
-                {
-                    operand1: 6,
-                    operand2: 3,
-                    operator: "-"
-                },
-                {
-                    operand1: 2,
-                    operand2: 3,
-                    operator: "*"
-                },
-                {
-                    operand1: 2,
-                    operand2: 3,
-                    operator: "/"
-                },
-            ],
-            currentQuestionIndex: 0
+            currentQuestionIndex: 0,
+            displayScoreCard: false
         }
+
+        this.questions = [
+            {
+                operand1: 2,
+                operand2: 3,
+                operator: "+"
+            },
+            {
+                operand1: 6,
+                operand2: 3,
+                operator: "-"
+            },
+            // {
+            //     operand1: 2,
+            //     operand2: 3,
+            //     operator: "*"
+            // },
+            // {
+            //     operand1: 2,
+            //     operand2: 3,
+            //     operator: "/"
+            // },
+        ];
 
         this.evaluatedAnswers = [];
     }
 
     onAnswerSubmitHandler = (answerDetails) => {
         answerDetails.index = this.state.currentQuestionIndex;
-        console.log(answerDetails);
+        // console.log(answerDetails);
         this.evaluatedAnswers.push(answerDetails);
 
         let newIndex = this.state.currentQuestionIndex + 1;
-        if (newIndex >= this.state.questions.length) {
-            this.onQuizFinishHandler();
+        if (newIndex >= this.questions.length) {
+            // this.onQuizFinishHandler();
+            this.setState({
+                displayScoreCard: true
+            })
         } else {
             this.setState({
                 currentQuestionIndex: newIndex
@@ -52,7 +58,7 @@ class Quiz extends Component {
     }
 
     onQuizFinishHandler = () => {
-        console.log("Quiz Finished.");
+        // console.log("Quiz Finished.");
     }
 
     render() {
@@ -60,10 +66,18 @@ class Quiz extends Component {
             <div className={classes.Quiz}>
                 <div className={classes.QuizHeader}>Quiz 1</div>
 
-                <QuizQuestion
-                    question={this.state.questions[this.state.currentQuestionIndex]}
-                    onAnswerSubmit={this.onAnswerSubmitHandler}
-                />
+                {this.state.displayScoreCard ? (
+                    <QuizScoreCard
+                        answerDetails={this.evaluatedAnswers}
+                        questions={this.questions}
+                    />
+                ) : (
+                        
+                    <QuizQuestion
+                        question={this.questions[this.state.currentQuestionIndex]}
+                        onAnswerSubmit={this.onAnswerSubmitHandler}
+                    />
+                )}
             </div>
         );
     }
